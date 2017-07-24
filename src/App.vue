99b17ce0349f89fe2dd719f67b3fdbe2a6778d62
mangoyi <759945821@qq.com>
2017-07-24 15:18:43 +0800

    add seller components

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,7 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import {urlParse} from 'common/js/util.js';
   import header from 'components/header/header.vue';
 
   const ERR_OK = 0;
@@ -24,15 +25,20 @@
   export default {
     data() {
       return {
-        seller: {}
+        seller: {
+          id: (() => {
+            let queryParm = urlParse();
+            return queryParm.id;
+          })()
+        }
       };
     },
     created() {
-      this.$http.get('/api/seller').then((response) => {
+      this.$http.get('/api/seller?id=' + this.seller.id).then((response) => {
         response = response.body;
         if (response.errno === ERR_OK) {
-          this.seller = response.data;
-        };
+          this.seller = Object.assign({}, this.seller, response.data);
+        }
       });
     },
     components: {
